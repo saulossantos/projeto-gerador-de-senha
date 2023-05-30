866d2ec870ec020d5a198f580d3decf3736e1397
saulossantos <saulosants@outlook.com>
2023-05-29 21:45:53 -0300

    Enviando atualizações

--- src/main.py
+++ src/main.py
@@ -1,16 +1,3 @@
-# gerador de senhas
-
-'''import random
-import string
-
-alfabeto = string.ascii_letters
-pontos = string.punctuation
-alfa_pont = alfabeto + pontos
-
-print("".join(random.SystemRandom().choices(alfa_pont, k=8)))'''
-
-
-# gerador de senhas
 
 import random
 import string
